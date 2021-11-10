f9cfc81ddaa8415787fd3dbc17d579ae9c54b21f
Wasiur Rahman <wasiurrahman195@gmail.com>
2021-11-10 23:26:04 +0600

    Add files via upload

---
+++ inc/header.php
@@ -0,0 +1,26 @@
+<<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="inc/bootstrap.min.css">
+<script src="inc/jquery.min.js"></script>
+<script src="inc/bootstrap.min.js"></script>
+<title>Title</title>
+</head>
+<body>
+    <div class="container">
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class= "navbar-brand"href="index.php">Login Register System PHP</a>
+
+                </div>
+                    <ul class="nav navbar-nav pull-right">
+                        <li><a href="profile.php">Profile</a></li>
+                        <li><a href="logout.php">Logout</a></li>
+                        <li><a href="login.php">Login</a></li>
+                        <li><a href="register.php">Register</a></li>
+                    </ul>
+                    
+            </div>
+        </nav>
